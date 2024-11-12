4c045065db6a76ed514b69950b62a0396ba36c98
ohmarsyxz <synce.2546@gmail.com>
2024-11-12 16:54:02 +0700

    assignment 1

--- mqtt_script_node_receiver/index.js
+++ mqtt_script_node_receiver/index.js
@@ -1,4 +1,5 @@
 const mqtt = require("mqtt");
+const { Client } = require("pg");
 
 // Connect to the MQTT broker
 const username = "username";
@@ -9,6 +10,30 @@ const client = mqtt.connect("ws://172.21.236.6:9001", {
   password,
 });
 const topic = "test/topic";
+const pgClient = new Client({
+  user: "user",
+  host: "db",
+  database: "mqtt_db",
+  password: "password",
+  port: 5432,
+});
+pgClient.connect();
+
+pgClient.query(
+  `
+  CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    msg TEXT
+  )
+`,
+  (err, res) => {
+    if (err) {
+      console.error("Error creating table:", err);
+    } else {
+      console.log("Table is ready");
+    }
+  }
+);
 
 // When a message is received
 client.on("connect", () => {
@@ -23,16 +48,19 @@ client.on("connect", () => {
   });
   client.on("message", (topic, message) => {
     console.log(`Message in topic ${topic} is: ${message}`);
+
+    const data = JSON.parse(message.toString());
+    pgClient
+      .query("INSERT INTO messages (msg) VALUES ($1)", [data.msg])
+      .then((res) => {
+        console.log("Data inserted successfully:", res);
+      })
+      .catch((err) => {
+        console.error("Error inserting data:", err);
+      });
   });
 });
-
 // Handle errors
 client.on("error", (err) => {
   console.error("Connection error:", err);
 });
-
-// Close the client after a delay (optional)
-// setTimeout(() => {
-//   client.end();
-//   console.log("Disconnected from MQTT broker");
-// }, 5000);
